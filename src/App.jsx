383407eaa0fc97f761adc6db16d544d0889407df
Kgaogelo <kgaogelojoseph33@gmail.com>
2025-01-28 06:23:55 -0800

    added hosting configurations

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Navbar from "./Components/Navbar";
-
+import Home from './Navbar-Components/Home';
 function App() {
   return (
     <> 
     <Navbar></Navbar> 
+    <Home/>
     </>
   );
 }
